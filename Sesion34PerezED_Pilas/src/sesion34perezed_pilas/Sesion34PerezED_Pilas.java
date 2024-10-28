96870ad275bae3876073ab5ad3657abcda4c0d94
zafkiel329 <gosh7287@gmail.com>
2024-10-28 14:34:13 -0600

    Confirmacion 1 OK Perez
    Datos de desarrollo: Brayan Perez

---
+++ Sesion34PerezED_Pilas/src/sesion34perezed_pilas/Sesion34PerezED_Pilas.java
@@ -0,0 +1,22 @@
+
+package sesion34perezed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+public class Sesion34PerezED_Pilas {
+
+    public static void main(String[] args) {
+        // Declaracion de pila
+        Stack<Integer> pilaPerez = new Stack<>();
+        
+        pilaPerez.push(100);
+        pilaPerez.push(150);
+        pilaPerez.push(230);
+        pilaPerez.push(140);
+        pilaPerez.push(160);
+        
+        JOptionPane.showMessageDialog(null, "Datos en pila ordenada es: \n"+pilaPerez);
+    }
+    
+}
